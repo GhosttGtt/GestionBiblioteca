e935fd47625f611dfb94405d7ef93717b6fa8001
D2005escobar10 <escobardavid2022@gmail.com>
2024-09-04 19:26:01 -0600

    Busqueda Filtrada

---
+++ FormularioBusqueda.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace biblioteca
+{
+    public partial class FormularioBusqueda : Form
+    {
+        private List<Libro> _libros;
+        public FormularioBusqueda(List<Libro> libros)
+        {
+            InitializeComponent();
+            _libros = libros;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string criterio = textBoxCriterio.Text.Trim();
+
+            if (string.IsNullOrEmpty(criterio))
+            {
+                MessageBox.Show("Ingrese un criterio de búsqueda");
+                return;
+            }
+            BusquedaFiltrada busquedaFiltrada;
+
+            if (radioButtonLibrosFisicos.Checked)
+            {
+                busquedaFiltrada = new BusquedaFiltradaLibroFisico(_libros);
+            }
+            else
+            {
+                busquedaFiltrada = new BusquedaFiltradaLibroElectronico(_libros);
+            }
+
+            List<Libro> resultados = busquedaFiltrada.Filtrar(criterio);
+
+            dgvlibros.DataSource = resultados;
+        }
+    }
+    }
+}
